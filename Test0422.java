a0a76d4b7da14a23d15d8365ca52629748e5c42c
joeyc09162 <joeyc09162@gmail.com>
2015-04-22 11:02:48 +0800

    AM021700

--- Test0422.java
+++ Test0422.java
@@ -1,9 +1,21 @@
-ï»¿public class Test0422{
+public class Test0422{
 	public static void main(String[] args){
-		System.out.println("20150422é–‹ç™¼æµç¨‹ç·´ç¿’");
+		System.out.println("20150422¶}µo¬yµ{½m²ß");
 		int x=10, y=5;
 		new A().minFunc(x, y);
 		new B().maxFunc(x, y);
 		new C().avgFunc(x, y);
+	}
+}
+
+class B{
+	public void maxFunc(int x,int y){
+		if(x>y){
+			System.out.println("max:"+x);
+		}
+	
+		if(x<y){
+			System.out.println("max:"+y);
+		}
 	}
 }
